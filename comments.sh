e426551714102196673d93cae66535edb8374a90
Dorinel Cristescu <dorinelcristescu@gmail.com>
2020-02-21 16:36:24 +0100

    Add comm script

---
+++ comments.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+# This line is intended to be used as a general description of the script
+# and anything that it does
+
+clear    # clears the screen
+
+MYUSERNAME="Terry"   # the username for this application
+
+echo "We are using the default user called: $MYUSERNAME" # display to the console
+
+DATETIMESTAMP=$(date)
+
+echo "This is when the script was run: $DATETIMESTAMP" # this is the timestamp of run
+
+
+
