6eab80f8940ea0c42c1824a873a795f62121c05f
Dorinel Cristescu <dorinelcristescu@gmail.com>
2020-03-08 20:05:56 +0100

    check remote servers connectivity

--- ping-script.sh
+++ ping-script.sh
@@ -2,7 +2,7 @@
 # Author: D
 # Description: This script will ping a remote host and notify
 
-ping -c 1 10.209.64.10
+ping -c 1 10.209.64.10 &> /dev/null
   if [ $? -eq 0 ];
     then
       echo OK
