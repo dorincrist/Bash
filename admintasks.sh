ab10b0712b3104636ab3a35e07943f33aa7b0f80
Dorinel Cristescu <dorinelcristescu@gmail.com>
2020-03-08 16:37:12 +0100

    run basic administration tasks

---
+++ admintasks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Author: Dorin
+# Date 08/03/2020
+# Description: This script will run basic admin commands
+
+echo This script will run basic administrative commands
+echo
+top | head -10
+echo
+df -h
+echo
+free -h
+echo
+uptime
+echo
+iostat
+echo
+echo End of Script
